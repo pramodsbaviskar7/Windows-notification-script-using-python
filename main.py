eadc2d4147bc5a95e1fa0ea95fbaa416242d32bc
Pramod Baviskar <pramodsbaviskar7@gmail.com>
2021-03-07 01:02:24 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+#pip install plyer
 from plyer import notification
 
 if __name__ == "__main__":
@@ -6,9 +7,12 @@ if __name__ == "__main__":
         notification.notify(
             title="Notification title",
             message="notification message here",
+            #you can add your own logo to the alert notification by providing location of the folder 
             app_icon="C:\Users\pramo\PycharmProjects\\notification\\appicon.ico",
+            #time of notification
             timeout=10
 
 
         )
+        #time of sleep i.e. upto which time you want the notfication to pop next
         time.sleep(6)
